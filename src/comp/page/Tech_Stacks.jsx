5f22743f9e4eea573dfdde613858f362664762be
RaniGiterman <rani.giterman10@gmail.com>
2022-08-22 11:54:10 +0300

    latest

--- src/comp/page/Tech_Stacks.jsx
+++ src/comp/page/Tech_Stacks.jsx
@@ -24,6 +24,7 @@ export default function Tech_Stacks() {
               <li className="m">React.js</li>
               <li className="m">Svelte.js</li>
               <li className="m">TailwindCSS</li>
+              <li className="m">Bootstrap</li>
             </ul>
           </div>
         </div>
