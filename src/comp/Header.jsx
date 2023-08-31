c4a26b6932029b54a508f3d77920115b3eccbba3
RaniGiterman <agiterman@gmail.com>
2023-08-31 18:47:05 +0300

    change main pic -w

--- src/comp/Header.jsx
+++ src/comp/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="float-right ml-12">
         <img src="Israel.png" className="w-24" alt="Israel Flag" />
       </div>
-      <div className="absolute left-2 top-28 sm:top-14 text-left text-2xl">
+      <div className="absolute left-2 top-28 sm:top-14 text-left text-xl sm:text-2xl">
         {/* Aspiring Engineer. */}
         Full Stack Engineer @{" "}
         <a href="https://www.icmega.co.il/home/">ICMega</a>.
