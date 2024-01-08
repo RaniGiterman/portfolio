164045f60edafb8fa689f2ff6d08f488a3103fb7
agiterman1 <agiterman@gmail.com>
2024-01-08 17:28:49 +0200

    tech tier list

--- src/comp/page/Tech_Stacks.jsx
+++ src/comp/page/Tech_Stacks.jsx
@@ -9,20 +9,20 @@ export default function Tech_Stacks() {
           <div>
             <h2 className="tech-title text-blue-600">Databases</h2>
             <ul>
-              <li className="m">MSSQL</li>
-              <li className="m">PostgreSQL</li>
-              <li className="m">Redis</li>
-              <li className="m">Memcached</li>
-              <li className="m">SQLite</li>
+              <li className="m W">MSSQL</li>
+              <li className="m W">PostgreSQL</li>
+              <li className="m W">Redis</li>
+              <li className="m mid">SQLite</li>
+              <li className="m L">Memcached</li>
             </ul>
           </div>
           <div>
             <h2 className="tech-title text-blue-600">Operating Systems</h2>
             <ul>
-              <li className="m">Pop!_OS</li>
-              <li className="m">Kali</li>
-              <li className="m">Windows</li>
-              <li className="m">MacOS</li>
+              <li className="m W">Pop!_OS (ubuntu)</li>
+              <li className="m W">Kali</li>
+              <li className="m mid">Windows</li>
+              <li className="m L">MacOS</li>
             </ul>
           </div>
         </div>
@@ -30,15 +30,15 @@ export default function Tech_Stacks() {
           <div>
             <h2 className="tech-title text-blue-600">High Level</h2>
             <ul>
-              <li className="m">HTML/CSS/JS</li>
-              <li className="m">Python</li>
-              <li className="m">Java</li>
+              <li className="m W">HTML/CSS/JS</li>
+              <li className="m W">Python</li>
+              <li className="m W">Java</li>
             </ul>
           </div>
           <div>
             <h2 className="tech-title text-blue-600">Proxy</h2>
             <ul>
-              <li className="m">HAProxy</li>
+              <li className="m W">HAProxy</li>
             </ul>
           </div>
         </div>
@@ -46,23 +46,24 @@ export default function Tech_Stacks() {
           <div>
             <h2 className="tech-title text-blue-600">Frameworks</h2>
             <ul>
-              <li className="m">React.js</li>
-              <li className="m">Svelte.js</li>
-              <li className="m">PIXI.js</li>
-              <li className="m">TailwindCSS</li>
-              <li className="m">Bootstrap</li>
-              <li className="m">Chart.js</li>
-              <li className="m">Node.js</li>
-              <li className="m">Express</li>
-              <li className="m">GoFiber</li>
-              <li className="m">gRPC</li>
+              <li className="m W">React.js</li>
+              <li className="m W">Node.js</li>
+              <li className="m W">Express</li>
+              <li className="m W">GoFiber</li>
+              <li className="m W">TailwindCSS</li>
+              <li className="m W">Bootstrap</li>
+              <li className="m W">QMK</li>
+              <li className="m mid">Svelte.js</li>
+              <li className="m mid">PIXI.js</li>
+              <li className="m mid">Chart.js</li>
+              <li className="m mid">gRPC</li>
             </ul>
           </div>
           <div>
             <h2 className="tech-title text-blue-600">Containers</h2>
             <ul>
-              <li className="m">Kubernetes</li>
-              <li className="m">Docker</li>
+              <li className="m mid">Kubernetes</li>
+              <li className="m W">Docker</li>
             </ul>
           </div>
         </div>
@@ -70,73 +71,33 @@ export default function Tech_Stacks() {
           <div>
             <h2 className="tech-title text-blue-600">Low Level</h2>
             <ul>
-              <li className="m">Rust</li>
-              <li className="m">C/C++</li>
-              <li className="m">Golang</li>
-              <li className="m">QMK</li>
+              <li className="m W">Golang</li>
+              <li className="m mid">Rust</li>
+              <li className="m mid">C/C++</li>
             </ul>
           </div>
           <div>
             <h2 className="tech-title text-blue-600">Tools</h2>
             <ul>
-              <li className="m">WireShark</li>
-              <li className="m">Git</li>
-              <li className="m">Jaeger</li>
-              <li className="m">Grafana</li>
-              <li className="m">Prometheus</li>
+              <li className="m W">WireShark</li>
+              <li className="m W">Git</li>
+              <li className="m W">Prometheus</li>
+              <li className="m W">Grafana</li>
+              <li className="m mid">Kibana</li>
+              <li className="m mid">Jaeger</li>
             </ul>
           </div>
         </div>
       </div>
 
-      {/* <div className="container-fluid text-center">
-        <div className="row">
-          <div className="col">
-            <h2 className="tech-title text-blue-600">Databases</h2>
-            <ul>
-              <li className="m">MSSQL</li>
-              <li className="m">PostgreSQL</li>
-              <li className="m">Redis</li>
-              <li className="m">Memcached</li>
-              <li className="m">SQLite</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h2 className="tech-title text-blue-600">Frameworks</h2>
-            <ul>
-              <li className="m">React.js</li>
-              <li className="m">Svelte.js</li>
-              <li className="m">PIXI.js</li>
-              <li className="m">TailwindCSS</li>
-              <li className="m">Bootstrap</li>
-              <li className="m">Chart.js</li>
-              <li className="m">Node.js</li>
-              <li className="m">Express</li>
-              <li className="m">GoFiber</li>
-              <li className="m">Prometheus</li>
-              <li className="m">Grafana</li>
-              <li className="m">gRPC</li>
-              <li className="m">Docker</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h2 className="tech-title text-blue-600">Languages</h2>
-            <ul>
-              <li className="m">HTML/CSS/JS</li>
-              <li className="m">Golang</li>
-              <li className="m">Python</li>
-              <li className="m">Java</li>
-              <li className="m">C</li>
-              <li className="m">HAProxy</li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
-      {/* <h2 className="tech-title text-blue-600">Personal Preference</h2> */}
       <br />
-      {/* <p className=" text-sm">
-        operating systems: Pop!_OS, Kali, Windows, MacOS
-      </p> */}
+      <p className="text-sm">
+        <strong className="m W">Green (Very Confident)</strong>
+        <br />
+        <strong className="m mid">Orange (Intermediate/In-between)</strong>
+        <br />
+        <strong className="m L">Red (Not Really Confident)</strong>
+      </p>
       <hr />
       <p>
         I go by the saying <i>"pick the right tool for the job"</i>. In that
